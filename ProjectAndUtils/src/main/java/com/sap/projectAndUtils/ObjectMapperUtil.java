68903044f8280b93e95b28e35abed1a795c0192d
I350644 <richard.xu03@sap.com>
2020-03-20 21:18:43 +0800

    Jackson usage

--- ProjectAndUtils/src/main/java/com/sap/projectAndUtils/ObjectMapperUtil.java
+++ ProjectAndUtils/src/main/java/com/sap/projectAndUtils/ObjectMapperUtil.java
@@ -9,10 +9,15 @@ import org.junit.Test;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 /**
  * refer to https://juejin.im/post/5d9733a26fb9a04e38584387
+ * https://www.history-of-my-life.com/archives/547
  * 
  * @author richard.xu03@sap.com
  * @version $Id: ObjectMapperUtil.java, v 0.1 Mar 12, 2020 2:34:47 PM richard.xu Exp $
@@ -32,9 +37,13 @@ public class ObjectMapperUtil {
         String intArray = "[1,2,3]";
         String booleanArray = "[true,false,true]";
 
-        System.out.println(objectMapper.readValue(stringArray, java.util.List.class));
-        System.out.println(objectMapper.readValue(intArray, java.util.List.class));
-        System.out.println(objectMapper.readValue(booleanArray, java.util.List.class));
+        List aList = objectMapper.readValue(stringArray, java.util.List.class);
+        System.out.println(objectMapper.writeValueAsString(aList));
+        List bList = objectMapper.readValue(intArray, java.util.List.class);
+        System.out.println(objectMapper.writeValueAsString(bList));
+        List cList = objectMapper.readValue(booleanArray, java.util.List.class);
+        System.out.println(objectMapper.writeValueAsString(cList));
+        objectMapper.readTree(stringArray);
     }
 
     /**
@@ -50,12 +59,50 @@ public class ObjectMapperUtil {
         String intArray = "[1,2,3]";
         String booleanArray = "[true,false,true]";
 
-        List<Object> aList = objectMapper.readValue(stringArray, new TypeReference<List<Object>>() {});
-        System.out.println(aList);
+        List<String> aList = objectMapper.readValue(stringArray, new TypeReference<List<String>>() {});
+        System.out.println(objectMapper.writeValueAsString(aList));
         List<Object> bList = objectMapper.readValue(intArray, new TypeReference<List<Object>>() {});
         System.out.println(bList);
         List<Object> cList = objectMapper.readValue(booleanArray, new TypeReference<List<Object>>() {});
         System.out.println(cList);
     }
 
+    /**
+     * JSON反序列化为Java对象
+     * 
+     * @throws JsonMappingException
+     * @throws JsonProcessingException
+     */
+    @Test
+    public void testReadJavaClass() throws JsonMappingException, JsonProcessingException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        String json = "{\"color\":\"Black\",\"type\":\"BMW\"}";
+        Car car = objectMapper.readValue(json, Car.class);
+        System.out.println(car);
+    }
+
+    /**
+     * JSON反序列化为JasonNode对象
+     * 
+     * @throws JsonMappingException
+     * @throws JsonProcessingException
+     */
+    @Test
+    public void testReadJsonNode() throws JsonMappingException, JsonProcessingException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        String json = "{ \"color\" : \"Black\", \"type\" : \"FIAT\" }";
+        JsonNode jsonNode = objectMapper.readTree(json);
+        String color = jsonNode.get("color").asText();
+        System.err.println(color);
+    }
+}
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+class Car {
+
+    private String color;
+    private String type;
 }
